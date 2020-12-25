bb3761994418732dc960263a1e42daf3d4619633
jacekku <kecajwy@gmail.com>
2020-12-25 20:13:13 +0100

    basic server logic

--- server.js
+++ server.js
@@ -2,14 +2,52 @@ const WebSocket = require('ws');
 const port = process.env.PORT || 8080
 const wss = new WebSocket.Server({ port });
 
-wss.on('connection', function connection(ws, req) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    wss.clients.forEach(ws => {
-      ws.send(message)
-    })
-  });
+let PLAYERS = []
 
+console.log('working on port ' + port)
+wss.on('connection', function connection(ws, req) {
+
+  ws.onmessage = (message) => {
+    const messageJson = JSON.parse(message.data)
+    const result = handleMessage(messageJson, ws)
+    if (!result) { ws.send("ERROR"); return }
+    if (result == "CONNECTED") ws.send("CONNECTED")
+    wss.clients.forEach(ws => ws.send(getPositionsString()))
+  }
+  ws.onclose = (event) => {
+    const disconnectedPlayer = event.target.player
+    PLAYERS = PLAYERS.filter(player => player.name != disconnectedPlayer)
+    wss.clients.forEach(ws => ws.send(getPositionsString()))
+
+  }
 });
 
-console.log('working')
+function handleMessage(messageJson, ws) {
+  if (!messageJson || !messageJson.type) {
+    return
+  }
+
+  if (messageJson.type == 'connect') {
+    const playerIndex = findPlayer(PLAYERS, messageJson.name)
+    if (playerIndex == -1) {
+      PLAYERS.push({ name: messageJson.name, position: messageJson.position })
+    }
+    ws.player = messageJson.name
+    return "CONNECTED"
+  }
+  if (messageJson.type == 'move') {
+    const playerIndex = findPlayer(PLAYERS, messageJson.name)
+    PLAYERS[playerIndex].position = messageJson.position
+    return "MOVED"
+  }
+}
+
+function findPlayer(array, name) {
+  return array.findIndex(item => item.name == name)
+}
+
+function getPositionsString() {
+  return JSON.stringify(PLAYERS)
+}
+
+
