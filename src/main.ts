f973f8b70a3a21e390688ee9ff0490d394623f55
jacekku <jacekwyroba97@gmail.com>
2021-08-27 23:17:14 +0200

    fix: fixing names and adding https handling

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('privkey.pem'),
+    cert: fs.readFileSync('fullchain.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
   app.useWebSocketAdapter(new IoAdapter(app));
   app.enableShutdownHooks();
