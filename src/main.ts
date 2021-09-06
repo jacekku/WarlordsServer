4fb5cda4499b1cabb2df4f297c5ebba36d6e2dc2
jacekku <jacekwyroba97@gmail.com>
2021-09-06 20:40:21 +0200

    fix(items): removing changes to test locally

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
-import { ItemParser } from './state/item-definitions/item-parser';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  // const httpsOptions = {
-  //   key: fs.readFileSync('/usr/warlords/privkey.pem'),
-  //   cert: fs.readFileSync('/usr/warlords/fullchain.pem'),
-  // };
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('/usr/warlords/privkey.pem'),
+    cert: fs.readFileSync('/usr/warlords/fullchain.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
   app.useWebSocketAdapter(new IoAdapter(app));
   app.enableShutdownHooks();
